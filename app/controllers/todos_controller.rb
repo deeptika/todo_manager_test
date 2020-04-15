b2a36d6f4c0d97a541789f32cde6916ccf7456c7
deeptika <deeptika24@gmail.com>
2020-04-15 19:24:55 +0530

    Updated create todo feature

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,5 +18,6 @@ class TodosController < ApplicationController
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     response_text = "Hey, your new todo has been created with id #{new_todo.id}"
+    render plain: response_text
   end
 end
