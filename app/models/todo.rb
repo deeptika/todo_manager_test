854f8c6cefeeaf46972d0ecdd218021443259141
deeptika <deeptika24@gmail.com>
2020-04-15 16:38:48 +0530

    Can see a single todo in /todo/:id

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,13 @@
 #todo.rb
 
 class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def to_pleasant_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
 end
