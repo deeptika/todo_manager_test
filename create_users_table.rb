08e87debad9234c65a48dcbb3398c7ed710be4a3
deeptika <deeptika24@gmail.com>
2020-04-16 14:46:08 +0530

    Created users table

---
+++ create_users_table.rb
@@ -0,0 +1,9 @@
+#create_users_table.rb
+require "./connect_db.rb"
+connect_db!
+
+ActiveRecord::Migration.create_table(:users) do |u|
+  u.column :name, :text
+  u.column :email, :text
+  u.column :password, :text
+end
